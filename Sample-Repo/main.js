c5543fa8df67028e7cb316e85eb51e97022bfc73
TsmHabib03 <114583236+TsmHabib03@users.noreply.github.com>
2025-08-31 16:03:46 +0800

    For and While Loop Array and Terrarium

---
+++ Sample-Repo/main.js
@@ -0,0 +1,44 @@
+// For Loop
+/*
+for(let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+  */
+
+// While Loop
+/*
+let i = 0;
+
+while(i <= 10){
+    console.log(i);
+    i++;
+}
+    */
+
+// Loop and Array
+// For Loop
+/*
+let missiles = ["AGM-114 Hellfire", "AGM-65 Maverick", "AGM-86 ALCM"];
+for(let i = 0; i < missiles.length; i++){
+    console.log(missiles[i]);
+}
+    */
+
+   // While Loop
+   /*
+   let attackHelicopters = ["AH-64 Apache", "AH-1 Cobra", "CH-53E Super Stallion"];
+   let i = 0;
+   while(i < attackHelicopters.length){
+    console.log(attackHelicopters[i]);
+    i++;
+   }
+   */
+   
+
+
+   //Create a program that lists every 3rd number between 1-20 and prints it to the console.
+   for(let i = 0 ; i <= 20; i+=3){
+    console.log(i);
+   }
+
+
